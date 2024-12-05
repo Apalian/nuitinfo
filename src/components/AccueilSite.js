a62ed903cb79b6535ac299d29017a4cbe5e7fe25
Sacha Pernak <sacha.pernak@etu.iut-tlse3.fr>
2024-12-05 19:39:06 +0100

    premier push

---
+++ src/components/AccueilSite.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AccueilSite() {
+    return (
+        <div>
+            <h1>Nouvelle Page</h1>
+            <p>Bienvenue sur la nouvelle page de notre application React.</p>
+        </div>
+    );
+}
+
+export default AccueilSite;
