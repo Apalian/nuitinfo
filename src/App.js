ffe510fb7a069843c41bb19ed5984dadf0591a92
Colin_lsplt <colinlespi@gmail.com>
2024-12-05 19:22:51 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Accueil from "./components/Accueil";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
       <Router>
+          <Navbar />
         <Routes>
           {/* Définir les routes */}
           <Route path="/" element={<Accueil />} />
