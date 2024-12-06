e2d3238957ba48b418589c80e30308dd9a49c167
Colin_lsplt <colinlespi@gmail.com>
2024-12-06 04:06:03 +0100

    Merge Sasha to Main

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import DetailsPage from "./components/DetailsPage";
 import MODX70Page from "./components/MODX70Page"
+import AccueilSite from "./components/AccueilSite";
 
 const App = () => {
   return (
       <Router>
-
         <Routes>
-          {/* Définir les routes */}
+          <Route path="/" element={<AccueilSite />} />
           <Route path="/MainPage" element={<MainPage />} />
-          <Route path="/DetailsPage" element={<DetailsPage />} />
-          <Route path="/MODX70Page" element={<MODX70Page />} />
         </Routes>
       </Router>
   );
